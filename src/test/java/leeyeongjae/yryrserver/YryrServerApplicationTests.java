78406ecc43b053f5239ee5075347d9b1d12126c6
LEEYEONGJAE1 <lyj8520675@naver.com>
2024-04-09 20:37:16 +0900

    asdf

--- src/test/java/leeyeongjae/yryrserver/YryrServerApplicationTests.java
+++ src/test/java/leeyeongjae/yryrserver/YryrServerApplicationTests.java
@@ -3,7 +3,6 @@ package leeyeongjae.yryrserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class YryrServerApplicationTests {
 
     @Test
