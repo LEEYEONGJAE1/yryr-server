779487bc45a317b00cb75bbeed22671f633d1b77
YEONGJAE LEE <lyj8520675@naver.com>
2024-04-10 20:57:41 +0900

    Asdf

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ec2-user/apps/yryr" #코드가 주입되는 경로
-JAR_FILE="$PROJECT_ROOT" #build.gradle에서 설정한 파일명으로 변경
+JAR_FILE="$PROJECT_ROOT/yuruyuri.jar" #build.gradle에서 설정한 파일명으로 변경
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
@@ -10,7 +10,7 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 TIME_NOW=$(date +%c)
 
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+cp $PROJECT_ROOT/build/libs/*.jar $PROJECT_ROOT
 
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
 nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
